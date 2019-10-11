188d84013903b6b7916aaee816e6ebc28c05c63b
liuchunliang <Chunliang.Liu@abcam.com>
2019-10-11 20:36:14 +0800

    获取打卡结果

--- lib/rails/ding/attendance_service.rb
+++ lib/rails/ding/attendance_service.rb
@@ -1,32 +1,6 @@
 module Rails
   module Ding
     class AttendanceService
-
-      # def self.createDept(accessToken, dept)
-      #   response = HttpService.post("/department/create?", {access_token: accessToken}, dept.to_json)
-      #   return response
-      # end
-      #
-      # def self.listDept(accessToken)
-      #   response = HttpService.get("/department/list?", {access_token: accessToken})
-      #   return response
-      # end
-      #
-      # def self.list_ids(accessToken)
-      #   response = HttpService.get("/department/list_ids?", {access_token: accessToken})
-      #   return response
-      # end
-      #
-      # def self.department_info(accessToken)
-      #   response = HttpService.get("/department/get?", {access_token: accessToken,id: 66950253})
-      #   return response
-      # end
-      #
-      # def self.deleteDept(accessToken, id)
-      #   response = HttpService.get("/department/delete?",{access_token: accessToken, id: id})
-      #   return response
-      # end
-
       # data = {
       #   "userIds": [1, 2, 3],
       #   "checkDateFrom": "2019-10-11 00:00:00",
@@ -34,12 +8,25 @@ module Rails
       #   "isI18n": "false"
       # }
       # listRecord(accessToken,data)
-
+      # 获取打卡详情
       def self.listRecord(accessToken, opt)
         response = HttpService.post("/attendance/listRecord",{access_token: accessToken}, opt.to_json)
         return response
       end
 
+      # data = {
+      #   "workDateFrom": "2019-10-11 00:00:00",
+      #   "workDateTo": "2019-10-11 00:00:00",
+      #   "userIdList": [1, 2, 3],
+      #   "offset": 0,
+      #   "limit": 1
+      # }
+      # listRecord(accessToken,data)
+      # 获取打卡结果
+      def self.list(accessToken, opt)
+        response = HttpService.post("/attendance/list",{access_token: accessToken}, opt.to_json)
+        return response
+      end
     end
   end
 end
